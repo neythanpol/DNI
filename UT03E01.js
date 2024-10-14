30edc95305b4a8179ecf98d26a561b66c8258777
neythanpol <ntn.blanco95@gmail.com>
2024-10-14 12:00:12 +0200

    ejercicio repetidos

---
+++ UT03E01.js
@@ -0,0 +1,6 @@
+// Escribe una función repetidos() que tome un array y devuelva otro array especificando el número de veces que se repite cada elemento del primer array.
+
+// Por ejemplo:    Si la entrada es [2, 3, 2, 2] la salida debe ser [3, 1]
+                // Si la entrada es [1, 1, 1] la salida deber ser [3]
+                // Si la entrada es [2, 3, 3, 2] la salida debe ser [2, 2]
+
